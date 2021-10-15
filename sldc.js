928f66fd41c1ce7b8173437148e304f2f3db9018
evaughtbpf <evaughtbpf@gmail.com>
2021-10-15 14:58:46 -0400

    First commit for Part 1

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let Planning
+let AnalysisofRequirements
+let Design
+let TestingIntegration
+let Implementation
+let Maintain
