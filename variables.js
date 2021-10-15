89c3c180d8b0f9e311c7a27a387894aab6b02eed
evaughtbpf <evaughtbpf@gmail.com>
2021-10-15 16:29:58 -0400

    First commit for Part 2 completed variables file questions

--- variables.js
+++ variables.js
@@ -1,17 +1,27 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+
+let myName = 'Erica'
+  console.log(myName)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+let faveNum = 444
+  console.log(faveNum)
+
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+let lovesCode = true
+  console.log(lovesCode)
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -22,9 +32,19 @@
 
 //CODE HERE
 
+if(faveNum === 13){
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13')
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for(i =0; i < 5; i++){ 
+  console.log(faveNum)
+}
