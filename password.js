76f900998ff331958ea7669dac4c0252f6f179da
evaughtbpf <evaughtbpf@gmail.com>
2021-10-16 15:59:51 -0400

    Second commit for password file. Code is not working.

--- password.js
+++ password.js
@@ -1,31 +1,48 @@
 
 //Note: the answer to this problem is not complete. I have a road block on figuring this out.
 
-const readline = require('readline');
+// console.log("Welcome to the password validator tool")
 
-const reader = readline.createInterface({
+// const readline = require('readline')
+
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// })
+
+// reader.question("What is your password?", function(input){
+//     password = input 
+//     console.log(input)
+
+//     if(`${password} ${password.lenght}` >= 10){
+//       console.log("Your password has been validated")
+//     }
+
+//     else{
+//       console.log("Your password can not be validated")
+//     }
+
+//     reader.close()
+// })
+
+console.log("Welcome to the password validator tool")
+
+const readline = require('readline')
+
+const password = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
+password.question("What is your password?", function(input){
 
-reader.question("what is your password?", function(input){
-    password = input;
-    console.log(input)
-
-    if(password.lenght <= 10){
-      console.log("Your password has been validated")
-    }
-
-    else{
-      console.log("Your password can not be validated")
-
-    }
-  
-	reader.close()
-
-
-});
-
+  if(password.lenght >= 10){
+    console.log("Your password has been validated")
+  }
+  else{
+    console.log("Your password can not be validated")
+  }
+  password.close()
+})
 
  
