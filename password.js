7f5e848f12300cfdcbf42497e1fe9ff9308467c9
evaughtbpf <evaughtbpf@gmail.com>
2021-10-15 22:35:22 -0400

    Second commit for the password file. Code is incomplete

--- password.js
+++ password.js
@@ -1,1 +1,31 @@
-console.log("Welcome to the password validator tool!")
+
+//Note: the answer to this problem is not complete. I have a road block on figuring this out.
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+reader.question("what is your password?", function(input){
+    password = input;
+    console.log(input)
+
+    if(password.lenght <= 10){
+      console.log("Your password has been validated")
+    }
+
+    else{
+      console.log("Your password can not be validated")
+
+    }
+  
+	reader.close()
+
+
+});
+
+
+ 
